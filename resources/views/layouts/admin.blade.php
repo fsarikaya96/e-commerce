6ae8267cc40e8876a66ab3c2c82cc52243c97aa1
Ferhat <fsarikaya96@hotmail.com>
2022-09-27 22:05:00 +0300

    more scripts bug fixed changes file js

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -35,21 +35,25 @@
 </div>
 </div>
 
-<script src="{{ asset("admin/vendors/base/vendor.bundle.base.js") }}"></script>
 
-<script src="{{ asset("admin/vendors/datatables.net/jquery.dataTables.js") }}"></script>
-<script src="{{ asset("admin/vendors/datatables.net-bs4/dataTables.bootstrap4.js") }}"></script>
 
-<script src="{{ asset("admin/js/off-canvas.js") }}"></script>
-<script src="{{ asset("admin/js/hoverable-collapse.js") }}"></script>
+<script src="{{ asset("assets/js/jquery-3.6.1.min.js") }}"></script>
+<script src="{{ asset("assets/js/bootstrap.bundle.min.js") }}"></script>
+{{--<script src="{{ asset("admin/vendors/base/vendor.bundle.base.js") }}"></script>--}}
+<script src="{{ asset("admin/vendors/ckeditor/ckeditor.js") }}"></script>
+{{--<script src="{{ asset("admin/vendors/datatables.net/jquery.dataTables.js") }}"></script>--}}
+{{--<script src="{{ asset("admin/vendors/datatables.net-bs4/dataTables.bootstrap4.js") }}"></script>--}}
 
-<script src="{{ asset("admin/js/template.js") }}"></script>
+{{--<script src="{{ asset("admin/js/off-canvas.js") }}"></script>--}}
+{{--<script src="{{ asset("admin/js/hoverable-collapse.js") }}"></script>--}}
 
-<script src="{{ asset("admin/js/dashboard.js") }}"></script>
+{{--<script src="{{ asset("admin/js/template.js") }}"></script>--}}
+
+{{--<script src="{{ asset("admin/js/dashboard.js") }}"></script>--}}
 {{--<script src="{{ asset("admin/js/data-table.js") }}"></script>--}}
 {{--<script src="{{ asset("admin/js/jquery.dataTables.js") }}"></script>--}}
 {{--<script src="{{ asset("admin/js/dataTables.bootstrap4.js") }}"></script>--}}
-<script src="{{ asset("admin/vendors/ckeditor/ckeditor.js") }}"></script>
+
 @vite('resources/js/admin.js')
 @livewireScripts
 @stack('script')
